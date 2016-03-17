f9d6d43f5122d0211e7128fe35ee4b7b94add17b
Marco Carrizales <marco_carrizales@msn.com>
2016-03-17 16:26:04 -0600

    Environment setup
    The libraries needed to start were imported.
    The icons were created.

---
+++ android/src/com/tamoxin/twofrogs/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tamoxin.twofrogs;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tamoxin.twofrogs.TwoFrogs;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TwoFrogs(), config);
+	}
+}
---
+++ desktop/src/com/tamoxin/twofrogs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tamoxin.twofrogs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tamoxin.twofrogs.TwoFrogs;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TwoFrogs(), config);
+	}
+}
